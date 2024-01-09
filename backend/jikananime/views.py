6fe71663015557d341757e76e375c3623f8ab24e
Warchafter <kevin.arriaga@gmail.com>
2024-01-09 06:50:14 -0500

    customary backup

--- backend/jikananime/views.py
+++ backend/jikananime/views.py
@@ -1,16 +1,9 @@
-from io import StringIO
 from django.shortcuts import get_object_or_404
-from django.core.paginator import Page
-from django.db.models import query
-from django.db.models.query import QuerySet
 from rest_framework.decorators import action
 from rest_framework.response import Response
-from rest_framework import viewsets, mixins, status, generics
-import jikananime
+from rest_framework import viewsets, mixins, status
 from rest_framework_simplejwt.authentication import JWTAuthentication
-from rest_framework_simplejwt.exceptions import InvalidToken
-from rest_framework.authentication import TokenAuthentication
-from rest_framework.permissions import IsAuthenticated, SAFE_METHODS, IsAdminUser, BasePermission
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.pagination import PageNumberPagination
 from rest_framework import filters
 
@@ -56,6 +49,20 @@ class StandardResultsSetPagination(PageNumberPagination):
             'count': self.page.paginator.count,
             'results': data
         })
+
+
+class JikanFavAnimeViewSet(viewsets.ViewSet):
+    permission_classes = [IsAuthenticated]
+    queryset = JikanFavoriteAnime.objects.all()
+
+    def list(self, request):
+        serializer_class = serializers.JikanFavoriteAnimeSerializer(self.queryset, many=True)
+        return Response(serializer_class.data)
+
+    def retrieve(self, requeist, pk=None):
+        fav_anime = get_object_or_404(self.queryset, pk=pk)
+        serializer_class = serializers.JikanFavoriteAnimeSerializer(fav_anime)
+        return Response(serializer_class.data)
 
 
 class JikanFavoriteAnimeViewSet(viewsets.ModelViewSet):
@@ -133,10 +140,3 @@ class JikanFavoriteAnimeViewSet(viewsets.ModelViewSet):
             return Response(f"{deleted_count} favorite anime(s) deleted successfully", status=status.HTTP_200_OK)
         else:
             return Response("No favorite anime deleted", status=status.HTTP_400_BAD_REQUEST)
-
-
-
-class TestFavoriteAnimeViewSet(viewsets.ModelViewSet):
-    queryset = JikanFavoriteAnimeViewSet
-    permission_classes = [IsAuthenticated]
-    serializer_class = serializers.TestFavoriteAnimeSerializer
