0078767d1a8a2308d120290cd233d3514327a55d
Warchafter <kevin.arriaga@gmail.com>
2024-01-11 16:06:16 -0500

    changed landing page1

---
+++ frontend/client/src/Containers/PlatformShowcaseV2.js
@@ -0,0 +1,54 @@
+import "./css/PlatformShowcase.css";
+import ReactLogo from '../assets/images/logo-react-icon.png';
+import PythonLogo from '../assets/images/python-logo.png';
+import JavascriptLogo from '../assets/images/JavaScript-Logo.png';
+import DockerLogo from '../assets/images/docker-logo.png';
+import ReduxLogo from '../assets/images/react-redux-logo.png'
+import PostgresLogo from '../assets/images/PostgreSQL-logo.png';
+import MaterialUILogo from '../assets/images/material-ui-logo.png';
+import DjangoLogo from '../assets/images/django-logo.jpg';
+
+
+
+const PlatformShowcase = () => {
+    return (
+        <div className='container-platforms'>
+            <div className='container-1 left'>
+                <div className='container-1-child'>
+                    <img className='skill-logo' src={JavascriptLogo} alt='js-logo'></img>
+                    <p>test</p>
+                </div>
+            </div>
+
+            <div className='container-1 right'>
+                <img className='skill-logo' src={PythonLogo} alt='python-logo'></img>
+            </div>
+
+            <div className='container-1 left'>
+                <img className='skill-logo' src={ReactLogo} alt='react-logo'></img>
+            </div>
+
+            <div className='container-1 right'>
+                <img className='skill-logo' src={DockerLogo} alt='docker-logo'></img>
+            </div>
+
+            <div className='container-1 left'>
+                <img className='skill-logo' src={ReduxLogo} alt='redux-logo'></img>
+            </div>
+
+            <div className='container-1 right'>
+                    <img className='skill-logo' src={PostgresLogo} alt='postgres-logo'></img>
+            </div>
+
+            <div className='container-1 left'>
+                <img className='skill-logo' src={MaterialUILogo} alt='material-ui-logo'></img>
+            </div>
+
+            <div className='container-1 right'>
+                <img className='skill-logo' src={DjangoLogo} alt='django-logo'></img>
+            </div>
+        </div>
+    )
+};
+
+export default PlatformShowcase;
